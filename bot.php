85bf08bd1089ec727bdda8f85b6f3d42e8d9f35d
kozlovcode <romandkoz@gmail.com>
2019-08-05 18:00:25 +0300

    Opt.

--- bot.php
+++ bot.php
@@ -1,27 +1,17 @@
 <?php
 
 define('TOKEN', '82');
-define('DOMAIN', 'https://example.com/');
-define('WEBHOOK', DOMAIN.'bot.php');
 define('CHATID', '35');
 define('API', 'https://api.telegram.org/bot'.TOKEN.'/');
 
-$result = file_get_contents(API.'getWebhookInfo');
-$result = json_decode($result);
-
-if (empty($result->result->url)) {
-	file_get_contents(API.'setWebhook?url='.WEBHOOK);
-	echo 'WEBHOOK IS INSTALLED';
-}
-
-$result = file_get_contents('php://input');
-$result = json_decode($result);
-switch ($result->message->text) {
+$data = file_get_contents('php://input');
+$data = json_decode($data);
+switch ($data->message->text) {
 	case '/start':
-		file_get_contents(API.'sendMessage?chat_id='.$result->message->chat->id.'&text=Напиши мне, а я передам все своему хозяину ');
+		file_get_contents(API.'sendMessage?chat_id='.$data->message->chat->id.'&text=Напиши мне, а я передам все своему хозяину ');
 		break;
 	default:
-		file_get_contents(API.'forwardMessage?chat_id='.CHATID.'&from_chat_id='.$result->message->chat->id.'&message_id='.$result->message->message_id);
-		file_get_contents(API.'sendMessage?chat_id='.$result->message->chat->id.'&text=Сообщение отправлено!');
+		file_get_contents(API.'forwardMessage?chat_id='.CHATID.'&from_chat_id='.$data->message->chat->id.'&message_id='.$data->message->message_id);
+		file_get_contents(API.'sendMessage?chat_id='.$data->message->chat->id.'&text=Сообщение отправлено!');
 		break;
 }
